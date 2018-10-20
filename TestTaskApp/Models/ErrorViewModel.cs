0b54d3a745a45fa02bf14693d38f5ce9cf776d89
a.matrosov <a.matrosov@AMATROSOV1068>
2018-10-20 14:43:21 +0300

    Добавьте файлы проекта.

---
+++ TestTaskApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestTaskApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
